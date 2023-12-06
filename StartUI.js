5d93ad1b634122bbef08c8d778f1485fdc1f2ab1
TorkRok <rt4537@student.uni-lj.si>
2023-12-06 20:30:05 +0100

    Title screen

---
+++ StartUI.js
@@ -0,0 +1,43 @@
+// StartUI.js
+export class StartUI {
+    constructor(startCallback) {
+        this.startCallback = startCallback;
+    }
+
+    show() {
+        // Create title screen elements
+        const titleScreen = document.createElement('div');
+        titleScreen.id = 'title-screen';
+        titleScreen.style.position = 'absolute';
+        titleScreen.style.top = '0';
+        titleScreen.style.width = '100%';
+        titleScreen.style.height = '100%';
+        titleScreen.style.display = 'flex';
+        titleScreen.style.flexDirection = 'column';
+        titleScreen.style.justifyContent = 'center';
+        titleScreen.style.alignItems = 'center';
+        titleScreen.style.backgroundColor = 'rgba(0, 0, 0, 0.85)';
+        titleScreen.style.color = 'white';
+        titleScreen.style.fontSize = '2em';
+
+        // Title
+        const title = document.createElement('h1');
+        title.textContent = 'My WebGPU Game';
+        titleScreen.appendChild(title);
+
+        // Start button
+        const startButton = document.createElement('button');
+        startButton.textContent = 'Start Game';
+        startButton.style.fontSize = '1em';
+        startButton.style.padding = '10px 20px';
+        startButton.style.cursor = 'pointer';
+        startButton.onclick = () => {
+            titleScreen.style.display = 'none';
+            this.startCallback();
+        };
+        titleScreen.appendChild(startButton);
+
+        // Append to body
+        document.body.appendChild(titleScreen);
+    }
+}
--- main.js
+++ main.js
@@ -7,6 +7,8 @@ import { OrbitController } from './common/engine/controllers/OrbitController.js'
 import { RotateAnimator } from './common/engine/animators/RotateAnimator.js';
 import { LinearAnimator } from './common/engine/animators/LinearAnimator.js';
 
+import { StartUI } from './StartUI.js';
+
 import {
     Camera,
     Model,
@@ -17,38 +19,50 @@ import {
 import { Renderer } from './Renderer.js';
 import { Light } from './Light.js';
 
-const canvas = document.querySelector('canvas');
-const renderer = new Renderer(canvas);
-await renderer.initialize();
 
-const gltfLoader = new GLTFLoader();
-await gltfLoader.load('common/models/monkey.gltf');
+document.addEventListener('DOMContentLoaded', () => {
+    // Create and show the start UI
+    const startUI = new StartUI(startGame);
+    startUI.show();
+});
 
-const scene = gltfLoader.loadScene(gltfLoader.defaultScene);
 
-const camera = scene.find(node => node.getComponentOfType(Camera));
-camera.addComponent(new OrbitController(camera, document.body, {
-    distance: 8,
-}));
+async function startGame() {
+    // Logic to start the game, such as initializing the Renderer and starting the game loop
+    const canvas = document.querySelector('canvas');
 
-const model = scene.find(node => node.getComponentOfType(Model));
+    const renderer = new Renderer(canvas);
+    await renderer.initialize();
 
-const light = new Node();
-light.addComponent(new Transform({
-    translation: [3, 3, 3],
-}));
-light.addComponent(new Light({
-    ambient: 0.1,
-}));
-light.addComponent(new LinearAnimator(light, {
-    startPosition: [3, 3, 3],
-    endPosition: [-3, -3, -3],
-    duration: 10,
-    loop: true,
-}));
-scene.addChild(light);
+    const gltfLoader = new GLTFLoader();
+    await gltfLoader.load('common/models/monkey.gltf');
 
-let startTime = Date.now();
+    const scene = gltfLoader.loadScene(gltfLoader.defaultScene);
+
+    const camera = scene.find(node => node.getComponentOfType(Camera));
+    camera.addComponent(new OrbitController(camera, document.body, {
+        distance: 8,
+    }));
+
+    const model = scene.find(node => node.getComponentOfType(Model));
+
+    const light = new Node();
+    light.addComponent(new Transform({
+        translation: [3, 3, 3],
+    }));
+    light.addComponent(new Light({
+        ambient: 0.1,
+    }));
+    light.addComponent(new LinearAnimator(light, {
+        startPosition: [3, 3, 3],
+        endPosition: [-3, -3, -3],
+        duration: 10,
+        loop: true,
+    }));
+    scene.addChild(light);
+
+
+    let startTime = Date.now();
 let frameCount = 0;
 let u_resolution = [0, 0];
 
@@ -75,3 +89,8 @@ function resize({ displaySize: { width, height }}) {
 
 new ResizeSystem({ canvas, resize }).start();
 new UpdateSystem({ update, render }).start();
+
+
+}
+
+
