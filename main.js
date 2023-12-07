38bf7e6fae7650f65e14d24a03bf2fe5d3b616e6
TorkRok <rt4537@student.uni-lj.si>
2023-12-07 23:05:59 +0100

    dodan start screen template

--- main.js
+++ main.js
@@ -32,105 +32,120 @@ import { CreateBullet } from './CreateBullet.js';
 import { SetStaticDynamic } from './SetStaticDynamic.js';
 import { BulletCollision } from './BulletCollision.js';
 
-const canvas = document.querySelector('canvas');
-const renderer = new Renderer(canvas);
-await renderer.initialize();
-
-const gltfLoader = new GLTFLoader();
-await gltfLoader.load('common/models/tank.gltf');
-
-const scene = gltfLoader.loadScene(gltfLoader.defaultScene);
-SetStaticDynamic(gltfLoader);
+import { StartUI } from '/StartUI.js';
+//import { GameUI } from './GameUI.js';
 
 
-// TANK
-const tank = gltfLoader.loadNode('telo');
-tank.isDynamic = true;
-tank.aabb = {
-    min: [-0.24, -0.2, -0.2],
-    max: [0.2, 0.2, 0.2],
-}
-tank.addComponent(new Controller(tank, document.body, {
-    distance: 2,
-}));
-const glava = gltfLoader.loadNode('glava');
-const top_glava = gltfLoader.loadNode('top_glava');
-glava.addComponent(new CevController(glava, top_glava, document.body));
-const camera = scene.find(node => node.getComponentOfType(Camera));
-glava.addChild(camera);
 
-const bullet = gltfLoader.loadNode('Sphere');
-scene.removeChild(bullet);
-//bullet.addComponent(new CreateBullet(gltfLoader, top_glava, scene));
+document.addEventListener('DOMContentLoaded', () => {
+    // Create and show the start UI
+    const startUI = new StartUI(startGame);
+    startUI.show();
 
-
-const light = new Node();
-light.addComponent(new Transform({
-    translation: [3, 3, 3],
-}));
-light.addComponent(new Light({
-    ambient: 0.7,
-}));
-scene.addChild(light);
-
-const physics = new Physics(scene);
-
-scene.traverse(node => {
-    const model = node.getComponentOfType(Model);
-    if (!model) {
-        return;
-    }
-
-    const boxes = model.primitives.map(primitive => calculateAxisAlignedBoundingBox(primitive.mesh));
-    node.aabb = mergeAxisAlignedBoundingBoxes(boxes);
 });
 
 
-//const cB = new CreateBullet(gltfLoader, top_glava, scene);
-let trk = { // naredimo object
-    boolTrk: false
-}
+async function startGame() {
 
-let nrTrk = 0;
+    const canvas = document.querySelector('canvas');
+    const renderer = new Renderer(canvas);
+    await renderer.initialize();
 
-CreateBullet(gltfLoader, top_glava, scene, trk);
+    const gltfLoader = new GLTFLoader();
+    await gltfLoader.load('common/models/tank.gltf');
 
-function update(time, dt) {
+    const scene = gltfLoader.loadScene(gltfLoader.defaultScene);
+    SetStaticDynamic(gltfLoader);
+
+
+    // TANK
+    const tank = gltfLoader.loadNode('telo');
+    tank.isDynamic = true;
+    tank.aabb = {
+        min: [-0.24, -0.2, -0.2],
+        max: [0.2, 0.2, 0.2],
+    }
+    tank.addComponent(new Controller(tank, document.body, {
+        distance: 2,
+    }));
+    const glava = gltfLoader.loadNode('glava');
+    const top_glava = gltfLoader.loadNode('top_glava');
+    glava.addComponent(new CevController(glava, top_glava, document.body));
+    const camera = scene.find(node => node.getComponentOfType(Camera));
+    glava.addChild(camera);
+
+    const bullet = gltfLoader.loadNode('Sphere');
+    scene.removeChild(bullet);
+    //bullet.addComponent(new CreateBullet(gltfLoader, top_glava, scene));
+
+
+    const light = new Node();
+    light.addComponent(new Transform({
+        translation: [3, 3, 3],
+    }));
+    light.addComponent(new Light({
+        ambient: 0.7,
+    }));
+    scene.addChild(light);
+
+    const physics = new Physics(scene);
+
     scene.traverse(node => {
-        for (const component of node.components) {
-            component.update?.(time, dt);
-            if (trk.boolTrk == true) {
-                nrTrk++;
-                console.log("WOOOOOO", nrTrk);
-            }
-            trk.boolTrk = false;
+        const model = node.getComponentOfType(Model);
+        if (!model) {
+            return;
         }
+
+        const boxes = model.primitives.map(primitive => calculateAxisAlignedBoundingBox(primitive.mesh));
+        node.aabb = mergeAxisAlignedBoundingBoxes(boxes);
     });
-    physics.update(time, dt);
-    //cB.update(time, dt);
-    //createBullet(bullet, top_glava, scene);
-    
+
+
+    //const cB = new CreateBullet(gltfLoader, top_glava, scene);
+    let trk = { // naredimo object
+        boolTrk: false
+    }
+
+    let nrTrk = 0;
+
+    CreateBullet(gltfLoader, top_glava, scene, trk);
+
+    function update(time, dt) {
+        scene.traverse(node => {
+            for (const component of node.components) {
+                component.update?.(time, dt);
+                if (trk.boolTrk == true) {
+                    nrTrk++;
+                    console.log("WOOOOOO", nrTrk);
+                }
+                trk.boolTrk = false;
+            }
+        });
+        physics.update(time, dt);
+        //cB.update(time, dt);
+        //createBullet(bullet, top_glava, scene);
+        
+    }
+
+    function render() {
+        renderer.render(scene, camera);
+    }
+
+    function resize({ displaySize: { width, height }}) {
+        camera.getComponentOfType(Camera).aspect = width / height;
+    }
+
+    new ResizeSystem({ canvas, resize }).start();
+    new UpdateSystem({ update, render }).start();
+
+    const gui = new GUI();
+    const controller = tank.getComponentOfType(Controller);
+    gui.add(controller, 'baseRotationSpeed', 0.0001, 0.01);
+    gui.add(controller, 'maxSpeed', 0, 200);
+    gui.add(controller, 'decay', 0, 1);
+    gui.add(controller, 'acceleration', 1, 100);
+
+
 }
-
-function render() {
-    renderer.render(scene, camera);
-}
-
-function resize({ displaySize: { width, height }}) {
-    camera.getComponentOfType(Camera).aspect = width / height;
-}
-
-new ResizeSystem({ canvas, resize }).start();
-new UpdateSystem({ update, render }).start();
-
-const gui = new GUI();
-const controller = tank.getComponentOfType(Controller);
-gui.add(controller, 'baseRotationSpeed', 0.0001, 0.01);
-gui.add(controller, 'maxSpeed', 0, 200);
-gui.add(controller, 'decay', 0, 1);
-gui.add(controller, 'acceleration', 1, 100);
-
-
-
 
 
